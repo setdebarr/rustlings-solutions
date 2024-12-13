6745f6236dca834a80337e2e9982f2b5e5b901e2
Sean DeBarr <12079581+setdebarr@users.noreply.github.com>
2024-12-13 16:20:34 -0500

    chore: update to rustlings v6.4.0
    Signed-off-by: Sean DeBarr <12079581+setdebarr@users.noreply.github.com>

--- exercises/08_enums/enums3.rs
+++ exercises/08_enums/enums3.rs
@@ -4,7 +4,6 @@ struct Point {
 }
 
 enum Message {
-    // TODO: Implement the message variant types based on their usage below.
     Resize { width: u64, height: u64 },
     Move(Point),
     Echo(String),
@@ -45,9 +44,8 @@ impl State {
     }
 
     fn process(&mut self, message: Message) {
-        // TODO: Create a match expression to process the different message variants.
-        // Remember: When passing a tuple as a function argument, you'll need extra parentheses:
-        // e.g. `foo((t, u, p, l, e))`
+        // TODO: Create a match expression to process the different message
+        // variants using the methods defined above.
         match message {
             Message::Resize { width, height } => self.resize(width, height),
             Message::ChangeColor(r, g, b) => self.change_color(r, g, b),
