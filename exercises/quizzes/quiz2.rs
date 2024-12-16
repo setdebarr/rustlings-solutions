e790f75c2489b971b0c074fca86b347628b0ed9e
Sean DeBarr <12079581+setdebarr@users.noreply.github.com>
2024-12-16 12:17:01 -0500

    quiz_2
    Signed-off-by: Sean DeBarr <12079581+setdebarr@users.noreply.github.com>

--- exercises/quizzes/quiz2.rs
+++ exercises/quizzes/quiz2.rs
@@ -27,7 +27,21 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function as described above.
-    // pub fn transformer(input: ???) -> ??? { ??? }
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+        let mut output = Vec::new();
+
+        for command in input {
+            match command.1 {
+                Command::Append(n) => output.push(command.0 + &"bar".repeat(n)),
+                Command::Trim => output.push(command.0.trim().to_string()),
+                Command::Uppercase => {
+                    output.push(command.0.to_uppercase());
+                }
+            }
+        }
+
+        output
+    }
 }
 
 fn main() {
@@ -37,7 +51,7 @@ fn main() {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    // use ???;
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]
